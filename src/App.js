aff31b4d0256429013a6f7059d0d1813e3f4bc66
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2024-08-15 05:48:48 +0000

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Loader from './pages/Loader';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/loader" element={
+          <>
+            <Helmet>
+              <title>Loader</title>
+            </Helmet>
+            <Loader />
+          </>
+        } />
+        <Route path="/" element={
+          <>
+            <Helmet>
+              <title>Signup</title>
+            </Helmet>
+            <Signup />
+          </>
+        } />
+        <Route path="/login" element={
+          <>
+            <Helmet>
+              <title>Login</title>
+            </Helmet>
+            <Login />
+          </>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
