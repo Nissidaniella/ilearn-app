6b1845ad5a8f63059c628fddd57f176a84bd3523
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2024-09-12 22:21:32 +0000

    updated profile pages

--- src/components/ProfileFront.js
+++ src/components/ProfileFront.js
@@ -12,7 +12,7 @@ const ProfileFront = ({
      
       
       <div className="profile-image">
-        <img src={profileImage} alt="User Profile" className='profile-image'/>
+        <img src={profileImage} alt="User Profile" />
       </div>
       <div className="user-name">     
         <h2>first name and last</h2></div>
