bea43c01c44361d5189e0725d8f3f5d08494d815
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2024-09-12 21:11:02 +0000

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,13 +22,12 @@ const Navbar = () => {
           </div>
 
           <div className="dropdown">
-          Hi user name!
+          
             <button className="dropbtn" onClick={toggleDropdown}>
             <FontAwesomeIcon icon={faBars} /> 
             </button>
             {isOpen && (
               <div className="dropdown-content">
-                <Link to="/profile">Profile</Link>
                 <Link to="/settings">Settings</Link>
                 <Link to="/login">Logout</Link>
               </div>
